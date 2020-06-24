669df738138282562fa32a4b2e6236cb375b8aa3
thaothao99 <17521069@gm.uit.edu.vn>
2020-06-24 17:03:33 +0700

    fix webpack

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'process';
 declare const module: any;
+const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT);
+  await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
